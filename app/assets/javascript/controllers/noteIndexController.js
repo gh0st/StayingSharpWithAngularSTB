680c03a6972e1bb4acbecb6de029febc3740fe71
Chad Hollman <hollmanchad@gmail.com>
2016-03-18 08:32:03 -0700

    Linking to notes, editing notes.

--- app/assets/javascript/controllers/noteIndexController.js
+++ app/assets/javascript/controllers/noteIndexController.js
@@ -1,12 +1,3 @@
 angular.module('NoteWrangler').controller('NotesIndexController', function(Note, $scope) {
     $scope.notes = Note.query();
-
-    console.log($scope.notes);
 });
-// angular.module('NoteWrangler').controller('NotesIndexController', function(Note, $scope) {
-//     console.log('My Controller got executed');
-//     Note.query().$promise.then(function(response) {
-//         $scope.notes = response;
-//         console.log(response);
-//     });
-// });
